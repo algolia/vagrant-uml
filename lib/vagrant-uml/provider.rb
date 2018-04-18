1027d1ec6c0681d39978933f7538e7b40c2681e8
Jeremy Jacque <jeremy.jacque@ymagis.com>
2018-04-19 00:35:17 +0200

    Return a valid vagrant state for the instance

--- lib/vagrant-uml/provider.rb
+++ lib/vagrant-uml/provider.rb
@@ -5,6 +5,7 @@ require 'vagrant/util/platform'
 module VagrantPlugins
   module UML
     class Provider < Vagrant.plugin("2", :provider)
+
       def initialize(machine)
         @logger  = Log4r::Logger.new("vagrant::provider::uml")
         if !Vagrant::Util::Platform.linux?
@@ -19,6 +20,9 @@ module VagrantPlugins
       end
 
       def action(name)
+        # Attempt to get the action method from the Action class if it
+        # exists, otherwise return nil to show that we don't support the
+        # given action.
         action_method = "action_#{name}"
         return Action.send(action_method, @machine) if Action.respond_to?(action_method)
         nil
@@ -33,8 +37,23 @@ module VagrantPlugins
       end
 
       def state
-        nil
+        env = @machine.action("read_state", lock: false)
+        state_id = env[:machine_state_id]
+        state_id = :unknown if !state_id
+
+        short = state_id.to_s.gsub("_", " ")
+        long  = I18n.t("vagrant.commands.status.#{state_id}")
+
+        # Return the MachineState object
+        Vagrant::MachineState.new(state_id, short, long)
       end
+
+      def to_s
+        id = @machine.id.nil? ? "new" : @machine.id
+        "UML (#{id})"
+      end 
+
+
     end
   end
 end
