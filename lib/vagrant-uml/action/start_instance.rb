201f8165500ecd2e127ce7f564d904ab41a921ac
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-04-19 17:37:22 +0200

    Use the UML arguments to start the kernel in a wiped env with the wanted rootfs

--- lib/vagrant-uml/action/start_instance.rb
+++ lib/vagrant-uml/action/start_instance.rb
@@ -12,10 +12,12 @@ module VagrantPlugins
         def call(env)
           env[:ui].info (I18n.t("vagrant_uml.starting"))
           run_pid = Process.spawn(
+            '/usr/bin/env',
             env[:machine].data_dir.to_s + "/run",
+            "ubda=cow,#{env[:uml_rootfs]} mem=256m eth0=tuntap,tap1,,192.168.1.254 con0=null,fd:2 con1=fd:0,fd:1 con=null ssl=null",
             :chdir => env[:machine].data_dir.to_s ,
-            :out => "/tmp/wtf.log,
-            :err => '/tmp/wtf_err.log")
+            :out => "wtf.log",
+            :err => "wtf_err.log")
           Process.detach run_pid
           env[:ui].success (I18n.t("vagrant_uml.started"))
           @app.call(env)
