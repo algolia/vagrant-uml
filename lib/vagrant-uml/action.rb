e10e90a35699c4ee1e9002c78ea7cbf07096ec8e
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-04-19 13:29:02 +0200

    Specify the Builtin module in some function calls

--- lib/vagrant-uml/action.rb
+++ lib/vagrant-uml/action.rb
@@ -11,16 +11,18 @@ module VagrantPlugins
 
       # This action brings the machine up from nothing, including creating the
       # container, configuring metadata, and booting.
-      def self.action_up(machine)
+      def self.action_up
         Builder.new.tap do |b|
+          b.use Builtin::ConfigValidate
           b.use Builtin::Call, Builtin::IsState, :not_created do |env, b2|
             # If the VM is NOT created yet, then do the setup steps
+            b2.use Builtin::Message, I18n.t("vagrant_uml.messages.not_created")
             if env[:result]
               b.use Builtin::HandleBox
               b.use HandleBoxMetadata
               #b.use CopyBox
+              #b.use Create
             end
-          b.use Builtin::ConfigValidate
           b.use StartInstance
         end
       end
@@ -28,7 +30,7 @@ module VagrantPlugins
       # This action is called to halt the machine.
       def self.action_halt
         Vagrant::Action::Builder.new.tap do |b|
-#          b.use ConfigValidate
+#          b.use Builtin::ConfigValidate
           b.use Call, IsCreated do |env, b2|
             if !env[:result]
               b2.use MessageNotCreated
@@ -45,7 +47,7 @@ module VagrantPlugins
         Vagrant::Action::Builder.new.tap do |b|
           b.use Call, DestroyConfirm do |env, b2|
             if env[:result]
-#             b2.use ConfigValidate
+#             b2.use Builtin::ConfigValidate
               b2.use Call, IsCreated do |env2, b3|
                 if !env2[:result]
                   b3.use MessageNotCreated
@@ -65,7 +67,7 @@ module VagrantPlugins
       # This action is called to SSH into the machine.
       def self.action_ssh
         Vagrant::Action::Builder.new.tap do |b|
-#          b.use ConfigValidate
+#          b.use Builtin::ConfigValidate
           b.use Call, IsCreated do |env, b2|
             if !env[:result]
               b2.use MessageNotCreated
@@ -79,7 +81,7 @@ module VagrantPlugins
 
       def self.action_ssh_run
         Vagrant::Action::Builder.new.tap do |b|
-#          b.use ConfigValidate
+#          b.use Builtin::ConfigValidate
           b.use Call, IsCreated do |env, b2|
             if !env[:result]
               b2.use MessageNotCreated
@@ -91,14 +93,26 @@ module VagrantPlugins
         end
       end
 
+      # This action is called to read the state of the machine. The
+      # resulting state is expected to be put into the `:machine_state_id`
+      # key.
+      def self.action_read_state
+        Vagrant::Action::Builder.new.tap do |b|
+          b.use Builtin::ConfigValidate
+          b.use ReadState
+        end
+      end
 
+      # The autoload farm
       action_root = Pathname.new(File.expand_path("../action", __FILE__))
       autoload :CopyBox, action_root.join("copy_box")
       autoload :StartInstance, action_root.join("start_instance")
-      autoload :StartInstance, action_root.join("stop_instance")
+      autoload :StopInstance, action_root.join("stop_instance")
+      autoload :HandleBoxMetadata, action_root.join("handle_box_metadata")
       autoload :MessageAlreadyCreated, action_root.join("message_already_created")
       autoload :MessageNotCreated, action_root.join("message_not_created")
       autoload :MessageWillNotDestroy, action_root.join("message_will_not_destroy")
+      autoload :ReadState, action_root.join("read_state")
     end
   end
 end
