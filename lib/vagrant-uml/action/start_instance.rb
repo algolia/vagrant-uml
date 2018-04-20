68122eb58bca471559de12d28a8932d9ef82d92c
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-04-20 08:58:52 +0200

    Add the umid arg when running the UML instance to allow later use of uml_console

--- lib/vagrant-uml/action/start_instance.rb
+++ lib/vagrant-uml/action/start_instance.rb
@@ -14,7 +14,7 @@ module VagrantPlugins
           run_pid = Process.spawn(
             '/usr/bin/env',
             env[:machine].data_dir.to_s + "/run",
-            "ubda=cow,#{env[:uml_rootfs]} mem=256m eth0=tuntap,tap1,,192.168.1.254 con0=null,fd:2 con1=fd:0,fd:1 con=null ssl=null",
+            "ubda=cow,#{env[:uml_rootfs]} umid=#{env[:machine].name} mem=256m eth0=tuntap,tap1,,192.168.1.254 con0=null,fd:2 con1=fd:0,fd:1 con=null ssl=null",
             :chdir => env[:machine].data_dir.to_s ,
             :out => "wtf.log",
             :err => "wtf_err.log")
