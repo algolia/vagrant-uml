fe4d05a19d45ae49f33d887c8dbf2160235a6e4f
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-04-20 10:50:07 +0200

    Rename the CopyBox action into a more \”standard" name: Create, dont forget to generate an uniq (?) id for the machine

--- lib/vagrant-uml/action/create.rb
+++ lib/vagrant-uml/action/create.rb
@@ -1,15 +1,18 @@
 module VagrantPlugins
   module UML
     module Action
-      class CopyBox
+      class Create
         def initialize(app, env)
           @app = app
         end
 
         def call(env)
+          config = env[:machine].provider_config
           env[:ui].info (I18n.t("vagrant_uml.copying"))
           FileUtils.cp_r(env[:machine].box.directory.to_s + "/metadata.json", env[:machine].data_dir)
           FileUtils.ln_s(env[:uml_kernel_bin], env[:machine].data_dir.to_s + "/run")
+          # Generate a random id for this machine
+          env[:machine].id=([*('a'..'z'),*('0'..'9')].shuffle[0,64].join.to_s)
           @app.call(env)
         end
       end
