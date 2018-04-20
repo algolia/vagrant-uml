5448f776652efdca753d0b3b375d435e6e9c74a5
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-04-20 14:06:11 +0200

    Use the process execution wrapper provided by the CLI class and handle errors garcefully

--- lib/vagrant-uml/action/start_instance.rb
+++ lib/vagrant-uml/action/start_instance.rb
@@ -6,19 +6,20 @@ module VagrantPlugins
     module Action
       class StartInstance
         def initialize(app, env)
+          @cli = CLI.new(env[:machine].name)
           @app = app
         end
 
         def call(env)
           env[:ui].info (I18n.t("vagrant_uml.starting"))
-          run_pid = Process.spawn(
-            '/usr/bin/env',
-            env[:machine].data_dir.to_s + "/run",
-            "ubda=cow,#{env[:uml_rootfs]} umid=#{env[:machine].name} mem=256m eth0=tuntap,tap1,,192.168.1.254 con0=null,fd:2 con1=fd:0,fd:1 con=null ssl=null",
-            :chdir => env[:machine].data_dir.to_s ,
-            :out => "wtf.log",
-            :err => "wtf_err.log")
-          Process.detach run_pid
+
+          begin
+            @cli.run( env[:machine].data_dir.to_s + "/run",
+              "ubda=cow,#{env[:uml_rootfs]} umid=#{env[:machine].name} mem=256m eth0=tuntap,tap1,,192.168.1.254 con0=null,fd:2 con1=fd:0,fd:1 con=null ssl=null")
+          rescue UML::Errors::ExecuteError => e
+            # Execution error, we were not able to start the UML instance
+            raise UML::Errors::StartError, exitcode: e.exitcode
+          end
           env[:ui].success (I18n.t("vagrant_uml.started"))
           @app.call(env)
         end
