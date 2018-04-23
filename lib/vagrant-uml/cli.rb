f3110ac9ee2fdf5d90991ad7ef6bd994edc1c4e1
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-04-23 18:03:29 +0200

    use the machine id as identifier for uml_mconsole and split arguments when calling process

--- lib/vagrant-uml/cli.rb
+++ lib/vagrant-uml/cli.rb
@@ -11,12 +11,15 @@ module VagrantPlugins
         @logger = Log4r::Logger.new("vagrant::uml::cli")
       end
 
-      def state
+      def state(id)
         if !@name
           return :unknown
         else
           begin
-            if @name && Process.run("uml_console" , @name , "version")
+            # This should be changed by a Process.new + process.run
+            # to specify some options
+            # Maybe use the standard Vagrant::Util::Subprocess class ... 
+            if @name && Process.excute("/usr/bin/uml_mconsole" , id , "version")
               if $1 =~ /^OK/
                 @logger.debug( "Cli.state: instance is running")
                 return :running
@@ -35,15 +38,20 @@ module VagrantPlugins
       end
 
       def run_uml(env,*command)
-        
         options = command.last.is_a?(Hash) ? command.pop : {}
         command = command.dup
 
-
+        # umdir should probably be set to data_dir to prevent zombies sockets 
         process = Process.new(
-          options[:data_dir] + "/run",
-          "ubda=cow,#{options[:rootfs]} umid=#{options[:machine_name]} mem=256m eth0=tuntap,tap1,,192.168.1.254 con0=null,fd:2 con1=fd:0,fd:1 con=null ssl=null",
-          :detach => true,
+          options[:data_dir] + "/run" ,
+          "ubda=cow,#{options[:rootfs]}" ,
+          "umid=#{options[:machine_id]}" ,
+          "mem=#{options[:mem]}m" ,
+          "eth0=tuntap,tap1,,192.168.1.254" ,
+          "con0=null,fd:1" ,
+          "con1=null,fd:2" ,
+          "con=pts ssl=null" ,
+          :detach => true ,
           :workdir => options[:data_dir]
         )
         process.run 
