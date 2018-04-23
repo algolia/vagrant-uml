f0a27d68372b515b126165a760771b824e6100ce
Jeremy Jacque <jeremy.jacque@ymagis.com>
2018-04-24 00:39:49 +0200

    Use standard vagrant subprocess for the state evaluation. Create the tun/tap device with hardcoded ip address ...

--- lib/vagrant-uml/cli.rb
+++ lib/vagrant-uml/cli.rb
@@ -16,23 +16,18 @@ module VagrantPlugins
           return :unknown
         else
           begin
-            # This should be changed by a Process.new + process.run
-            # to specify some options
-            # Maybe use the standard Vagrant::Util::Subprocess class ... 
-            if @name && Process.excute("/usr/bin/uml_mconsole" , id , "version")
-              if $1 =~ /^OK/
-                @logger.debug( "Cli.state: instance is running")
-                return :running
-              else
-                @logger.debug( "Cli.state: instance is in unknown state")
-                return :unknown
-              end
+            res = Vagrant::Util::Subprocess.execute("uml_mconsole", id, "version", retryable: true)
+            if res.stdout =~ /^OK/
+              @logger.debug( "Cli.state: instance is running")
+              return :running
+            else
+              return :not_running
             end
           rescue
             ## We should try to figure out if the machine is stopped, not_created, ...
             #   test if the cow file exists ?
-            @logger.debug( "Cli.state: instance is not running")
-            return :not_running
+            @logger.debug( "Cli.state: RESCUE instance state unknown error during call to uml_mconsole")
+            return :unknow
           end
         end
       end
@@ -41,13 +36,16 @@ module VagrantPlugins
         options = command.last.is_a?(Hash) ? command.pop : {}
         command = command.dup
 
+        res = Vagrant::Util::Subprocess.execute("tunctl", "-t", options[:machine_id], retryable: true)
+        res2 = Vagrant::Util::Subprocess.execute("ifconfig", options[:machine_id], "192.168.1.254" , "up", retryable: true)
+
         # umdir should probably be set to data_dir to prevent zombies sockets 
         process = Process.new(
           command[0],
           "ubda=cow,#{options[:rootfs]}" ,
           "umid=#{options[:machine_id]}" ,
           "mem=#{options[:mem]}m" ,
-          "eth0=tuntap,tap1,,192.168.1.254" ,
+          "eth0=tuntap,#{options[:machine_id]},,192.168.1.254" ,
           "con0=null,fd:1" ,
           "con1=null,fd:2" ,
           "con=pts ssl=null" ,
