bdd9e1b2b983e377168bb26034962133a2410353
Jeremy Jacque <jeremy.jacque@ymagis.com>
2018-04-29 02:57:05 +0200

    Always generate a mac address on instance creation

--- lib/vagrant-uml/action/create.rb
+++ lib/vagrant-uml/action/create.rb
@@ -15,16 +15,14 @@ module VagrantPlugins
           mac_octets[0] |= 0x02
           (1..3).each { mac_octets << rand(256) }
           mac = mac_octets.collect { |i| format % [i] }.join(delimiter)
+          mac.to_s
         end
 
         def call(env)
-          
-          if !env[:machine].provider_config.mac
-            env[:machine].provider_config.mac = generate_mac
-            mac_file = env[:machine].data_dir.join("action_create"
-            mac_file.open("w") do |f|
-              f.write(env[:machine].provider_config.mac)
-            end
+          env[:machine].provider_config.mac = generate_mac
+          mac_file = env[:machine].data_dir.join("action_create")
+          mac_file.open("w") do |f|
+            f.write(env[:machine].provider_config.mac)
           end
 
           env[:ui].info (I18n.t("vagrant_uml.copying"))
