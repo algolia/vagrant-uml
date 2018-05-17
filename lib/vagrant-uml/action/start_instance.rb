95bd60824a2421134d23f1e3a3da088febe946a7
Jeremy Jacque <jeremy.jacque@ymagis.com>
2018-05-18 00:08:57 +0200

    Use the standalone network config with (temporarly) hard-coded ip address. Increase available ressouces in the UML instance

--- lib/vagrant-uml/action/start_instance.rb
+++ lib/vagrant-uml/action/start_instance.rb
@@ -13,15 +13,16 @@ module VagrantPlugins
         def call(env)
           env[:ui].info (I18n.t("vagrant_uml.starting"))
           env[:machine].provider.capability(:nic_mac_address)
-          @cli.create_standalone_net(:name => env[:machine].id,:host_ip => "10.0.3.1")
+          @cli.create_standalone_net(:name => env[:machine].id,:host_ip => "192.168.0.1")
           begin
             @cli.run_uml( env[:machine].data_dir.to_s + "/run",
               :data_dir => env[:machine].data_dir.to_s,
               :machine_id => env[:machine].id,
               :rootfs => env[:uml_rootfs],
-              :mem => 256,
-              :cpu => 1,
-              :eth0 => "daemon,#{env[:machine].provider_config.mac},unix,/tmp/uml_switch.ctl",
+              :mem => 1024,
+              :ncpus => 2,
+#              :eth0 => "daemon,#{env[:machine].provider_config.mac},unix,/tmp/uml_switch.ctl",
+              :eth0 => "tuntap,#{env[:machine].id},#{env[:machine].provider_config.mac},192.168.0.1",
               :con0 => "null,fd:1",
               :con1 => "null,fd:2",
               :con => "pts",
