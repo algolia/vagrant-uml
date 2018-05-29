34c67602b427d6523e4ad0cfbe7ae970c8aabeeb
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-05-29 14:15:35 +0200

    Store the host ip associated with this instance in the global machine index. Use this to detect the already existing ip address used by UML

--- lib/vagrant-uml/action/create.rb
+++ lib/vagrant-uml/action/create.rb
@@ -5,6 +5,7 @@ module VagrantPlugins
         def initialize(app, env)
           @app = app
           @cli = CLI.new(env[:machine].name)
+          @logger = Log4r::Logger.new("vagrant::uml::action::create")
         end
 
 
@@ -15,25 +16,44 @@ module VagrantPlugins
           mac_octets = (1..6).collect { rand(256) }
           # Set the locally administered bit
           mac_octets[0] |= 0x02
-          # Unsure this is not a multicast mac address by seting the LSB to 0
+          # Ensure this is not a multicast mac address by seting the LSB to 0
           mac_octets[0] -=1 if mac_octets[0].odd?
           mac = mac_octets.collect { |i| format % [i] }.join(delimiter)
           mac.to_s
         end
 
         def call(env)
+ 
+          data_dir = env[:machine].data_dir
+
           env[:machine].provider_config.mac = generate_mac
-          mac_file = env[:machine].data_dir.join("action_create")
+          mac_file = data_dir.join("action_create")
           mac_file.open("w") do |f|
             f.write(env[:machine].provider_config.mac)
           end
 
           env[:ui].info (I18n.t("vagrant_uml.copying"))
-          FileUtils.cp_r(env[:machine].box.directory.to_s + "/metadata.json", env[:machine].data_dir)
-          FileUtils.ln_s(env[:uml_kernel_bin], env[:machine].data_dir.to_s + "/run")
+          FileUtils.cp_r(env[:machine].box.directory.to_s + "/metadata.json", data_dir)
+          FileUtils.ln_s(env[:uml_kernel_bin], data_dir.to_s + "/run")
           # Generate a random id for this machine
           env[:machine].id=([*('a'..'z'),*('0'..'9')].shuffle[0,15].join.to_s)
-          @cli.create_cidata(:root_path => env[:machine].env.root_path.to_s, :machine_id => env[:machine].id, :name => env[:machine].name, :mac => env[:machine].provider_config.mac, :data_dir => env[:machine].data_dir.to_s)
+
+          # Check existing uml instance for ip address
+          env[:machine].env.machine_index.each |entry| do
+            if entry.provider == "uml" && entry.id != env[:machine].index_uuid
+              @logger.info("Existing UML ip: #{entry.extra_data["host_ip"]}")
+            end
+          end
+
+          # Create a cloud-init seed image
+          @cli.create_cidata(:root_path => env[:machine].env.root_path.to_s, :machine_id => env[:machine].id, :name => env[:machine].name, :mac => env[:machine].provider_config.mac, :data_dir => data_dir.to_s)
+
+          # Store the host ip associated with this instance in the global machine index
+          entry = env[:machine].env.machine_index.get(env[:machine].index_uuid)
+          entry.extra_data["host_ip"] = "192.168.0.2"
+          entry = env[:machine].env.machine_index.set(entry)
+          entry = env[:machine].env.machine_index.release(entry)
+
           @app.call(env)
         end
       end
