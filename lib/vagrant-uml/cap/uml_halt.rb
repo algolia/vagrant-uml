4b7e89d61d78f5a9d99b469d26b10c36dfdbc7cd
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-06-01 12:02:17 +0200

    We want to shutdown the instance not reboot it in the uml_halt capability

--- lib/vagrant-uml/cap/uml_halt.rb
+++ lib/vagrant-uml/cap/uml_halt.rb
@@ -2,9 +2,9 @@ module VagrantPlugins
   module UML
     module Cap
       class UMLHalt
-        def self.halt(machine)
+        def self.uml_halt(machine)
           begin
-            machine.communicate.sudo("{ sleep 1; reboot -f; } >/dev/null &")
+            machine.communicate.sudo("{ sleep 1; shutdown -h now; } >/dev/null &")
           rescue IOError, Vagrant::Errors::SSHDisconnected
             # Do nothing, because it probably means the machine shut down
             # and SSH connection was lost.
