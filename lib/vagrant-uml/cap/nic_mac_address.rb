261d2f4adac1a02f7742eade16da3b75936dfd34
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-06-01 11:16:17 +0200

    reorganize capabilities in a subdir with subclass

--- lib/vagrant-uml/cap/nic_mac_address.rb
+++ lib/vagrant-uml/cap/nic_mac_address.rb
@@ -1,14 +1,16 @@
 module VagrantPlugins
   module UML
-    module Cap 
-      # Reads the network interface card MAC addresses and returns them.
-      #   
-      # @return [Hash<String, String>] Adapter => MAC address
-      def self.nic_mac_address(machine)
-        mac_file = machine.data_dir.join("action_create")
-        machine.provider_config.mac = mac_file.read.chomp if mac_file.file?
-      end 
+    module Cap
+      module NicMacAddress
 
+        # Reads the network interface card MAC addresses and returns them.
+        #   
+        # @return [Hash<String, String>] Adapter => MAC address
+        def self.nic_mac_address(machine)
+          mac_file = machine.data_dir.join("action_create")
+          machine.provider_config.mac = mac_file.read.chomp if mac_file.file?
+        end 
+      end
     end 
   end 
 end
