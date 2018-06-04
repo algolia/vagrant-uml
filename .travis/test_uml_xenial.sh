6febcdba0b8f1e373bbaa9fe7c99a853c579d7a8
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-06-04 15:55:04 +0200

    Run bundler install and add some output

--- .travis/test_uml_xenial.sh
+++ .travis/test_uml_xenial.sh
@@ -2,10 +2,19 @@
 
 set -e
 
+echo "Running bundler install"
+bundler install
+
+echo "Running rake"
 rake build
+
+echo "Installing vagrant plugin"
 vagrant plugin install ./pkg/vagrant-uml-0.0.2.gem
+
+echo "Getting Vagrant UML box"
 vagrant box add https://alg-archistore.s3.amazonaws.com/public/infra/vagrant/uml/xenial64/algolia_uml_xenial64-0.0.2.box
 
+echo "Try the UML instance"
 mkdir /tmp/instance
 pushd /tmp/instance
 vagrant init algolia/uml/xenial64
