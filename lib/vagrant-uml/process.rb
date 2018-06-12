ed8bf6e0c59e7808dfa251d1d2c2960b9fd35b51
Jeremy Jacque <jeremy.jacque@algolia.com>
2018-06-12 14:56:41 +0200

    Reorganize code to make it easier to read

--- lib/vagrant-uml/process.rb
+++ lib/vagrant-uml/process.rb
@@ -51,24 +51,16 @@ module VagrantPlugins
       #       paste from VirtualBox base driver and has no tests
       def execute(*command, &block)
         tries = 0
-        tries = 3 if @options[:retryable]
-        # Ensure detachable process are ,not retryable
-        tries = 0 if @options[:detach]
+        tries = 3 if @options[:retryable] && !@options[:detach]
 
         sleep = @options.fetch(:sleep, 1)
 
         # Variable to store our execution result
         r = nil
-
         retryable(on: UML::Errors::ExecuteError, tries: tries, sleep: sleep) do
           # Execute the command
           r = raw(*command, &block)
 
-          ################################################################################
-          ## Need to change this as the uml_console XXXX version return 1 when the machine
-          ## does not exists (need to rely on something to get the status)
-          ##  the best way would be to add a "status" command to uml_console to know is the
-          ##  machine exists, is frozen , ...
           # If the command was a failure, then raise an exception that is
           # nicely handled by Vagrant.
           if r.exit_code != 0 && r.exited? && !@options[:detach]
@@ -76,23 +68,13 @@ module VagrantPlugins
               raise UML::Errors::SubprocessInterruptError, command.inspect
             else
               raise UML::Errors::ExecuteError,
-                command: command.inspect, stderr: r.io.stderr, stdout: r.io.stdout, exitcode: r.exit_code
+                command: command.inspect,
+                stderr: r.io.stderr,
+                stdout: r.io.stdout,
+                exitcode: r.exit_code
             end
           end
-          ###################################################################################
         end
-
-        ################################################################################
-        ## We probably dont care about this as UML is only running on Linux hosts !
-        # Return the output, making sure to replace any Windows-style
-        # newlines with Unix-style.
-        #        stdout = r.stdout.gsub("\r\n", "\n")
-        #        if options[:show_stderr]
-        #          { :stdout => stdout, :stderr => r.stderr.gsub("\r\n", "\n") }
-        #        else
-        #          stdout
-        #        end
-        ###################################################################################
         return r
       end
 
